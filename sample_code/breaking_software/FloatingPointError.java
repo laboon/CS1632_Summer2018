088a880c9af7070c3aee96ca6ee8cd98bd0bc7b7
laboon <laboon@gmail.com>
2018-05-23 09:33:33 -0400

    add rubocop config

--- sample_code/breaking_software/FloatingPointError.java
+++ sample_code/breaking_software/FloatingPointError.java
@@ -1,7 +1,7 @@
 public class FloatingPointError {
 
     public static void main(String[] args) {
-	final double SOME_VALUE = 857.0;
+	final double SOME_VALUE = 857.0; // THIS IS A LINE
 	double oneVal = 1.0 / SOME_VALUE;
 	double total = oneVal * SOME_VALUE;
 
